c8ff95300ece3ead0b5503714c0851bcb50d0da7
Gitabhi07 <88783656+Gitabhi07@users.noreply.github.com>
2025-03-04 06:47:10 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -6,7 +6,19 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", () => {
+      console.log("Error: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGoDB connection failed !!!", err);
+  });
 
 /*
 import express from "express";
